9e2bfcfec7aa9c4e65da8a76b41ba496b3e90040
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-07-05 19:44:20 +0400

    add more items to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,75 @@ Job.create(description:'We are rapidly expanding our engineering teams and are l
             position:'Software Engineer',
             date:'2021-06-29',
             logo:'https://remoteok.io/assets/jobs/2e2fd6dc27b9e979a725e226ab90afc81624986950.png')
+            
+Job.create(description:'We would like you to provide a Frontend (Web or Mobile) where the user can scroll through a list of job openings. If',
+            company:'Beekin',
+            location:'USA',
+            technologies:['sysadmin','sysctl','sales'],
+            position:'Director Sales Enablement',
+            date:'2021-06-29',
+            logo:'https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png')
+
+Job.create(description:'Implement the solution to the highest standards while adhering to industry accepted architectureand design pattern techniques and methodologies',
+            company:'Labelbox',
+            location:'Worldwide',
+            technologies:['Python','Javascript','Bash','R'],
+            position:'Senior Product Manager',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/c9815226a03ed170fb2e53534483c7cc1624924807.png')
+
+Job.create(description:'We are rapidly expanding our engineering teams and are looking for hungry,curious, fast-learning engineers for all levels and teams who aren’t afraid of challenges!',
+            company:'Gro Intelligence',
+            location:'Remote',
+            technologies:['PostgreSQL','Django','Redis','AWS'],
+            position:'Software Engineer',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/2e2fd6dc27b9e979a725e226ab90afc81624986950.png')
+
+Job.create(description:'We would like you to provide a Frontend (Web or Mobile) where the user can scroll through a list of job openings. If',
+            company:'Beekin',
+            location:'USA',
+            technologies:['sysadmin','sysctl','sales'],
+            position:'Director Sales Enablement',
+            date:'2021-06-29',
+            logo:'https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png')
+
+Job.create(description:'Implement the solution to the highest standards while adhering to industry accepted architectureand design pattern techniques and methodologies',
+            company:'Labelbox',
+            location:'Worldwide',
+            technologies:['Python','Javascript','Bash','R'],
+            position:'Senior Product Manager',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/c9815226a03ed170fb2e53534483c7cc1624924807.png')
+
+Job.create(description:'We are rapidly expanding our engineering teams and are looking for hungry,curious, fast-learning engineers for all levels and teams who aren’t afraid of challenges!',
+            company:'Gro Intelligence',
+            location:'Remote',
+            technologies:['PostgreSQL','Django','Redis','AWS'],
+            position:'Software Engineer',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/2e2fd6dc27b9e979a725e226ab90afc81624986950.png')
+
+Job.create(description:'We would like you to provide a Frontend (Web or Mobile) where the user can scroll through a list of job openings. If',
+            company:'Beekin',
+            location:'USA',
+            technologies:['sysadmin','sysctl','sales'],
+            position:'Director Sales Enablement',
+            date:'2021-06-29',
+            logo:'https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png')
+
+Job.create(description:'Implement the solution to the highest standards while adhering to industry accepted architectureand design pattern techniques and methodologies',
+            company:'Labelbox',
+            location:'Worldwide',
+            technologies:['Python','Javascript','Bash','R'],
+            position:'Senior Product Manager',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/c9815226a03ed170fb2e53534483c7cc1624924807.png')
+
+Job.create(description:'We are rapidly expanding our engineering teams and are looking for hungry,curious, fast-learning engineers for all levels and teams who aren’t afraid of challenges!',
+            company:'Gro Intelligence',
+            location:'Remote',
+            technologies:['PostgreSQL','Django','Redis','AWS'],
+            position:'Software Engineer',
+            date:'2021-06-29',
+            logo:'https://remoteok.io/assets/jobs/2e2fd6dc27b9e979a725e226ab90afc81624986950.png')
